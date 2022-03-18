a63503204b0705faff373b5351ba9f14ad63186f
Alexandr Solomevich <alexandr.solomevich@gmail.com>
2022-03-18 23:03:48 +0300

    created min_max func lab2 os

--- Operating Systems/Lab2/main.cpp
+++ Operating Systems/Lab2/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+struct arrStruct {
+    arrStruct(int size, int *array) : size(size), array(array) {}
+
+    int size;
+    int* array;
+    int min;
+    int max;
+    int average;
+};
+
+DWORD WINAPI min_max(LPVOID lpParam) {
+    arrStruct* arrSt = (arrStruct*)lpParam;
+
+    arrSt->min = arrSt->max = arrSt->array[0];
+    for (int i = 0; i < arrSt->size; i++) {
+        if (arrSt->array[i] > arrSt->max)
+            arrSt->max = arrSt->array[i];
+        Sleep(7);
+        if (arrSt->array[i] < arrSt->min)
+            arrSt->min = arrSt->array[i];
+        Sleep(7);
+    }
+
+    cout << "Max element: " << arrSt->max << '\n';
+    cout << "Min element: " << arrSt->min << '\n';
+
+    return 0;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
