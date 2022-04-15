d89bd0af46dd1c55f0e88ef928434d39149f5654
Alexandr Solomevich <alexandr.solomevich@gmail.com>
2022-04-15 22:39:18 +0300

    initial commit lab3 os

---
+++ Operating Systems/Lab3/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int* array = new int[n];
+    for (int i = 0; i < n; i++) {
+        array[i] = 0;
+    }
+
+    int n_marker;
+    cin >> n_marker;
+
+    return 0;
+}
