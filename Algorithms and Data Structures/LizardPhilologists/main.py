cf484d8e4c4d257eef2709567f564366d73ecdd2
Alexandr Solomevich <alexandr.solomevich@gmail.com>
2022-05-23 01:49:42 +0300

    pass 316/403 tests

---
+++ Algorithms and Data Structures/LizardPhilologists/main.py
@@ -0,0 +1,39 @@
+n, k = map(int, input().split())
+
+chu = [1, 3, 8]
+ka = [1, 3, 8]
+pi = [1, 3, 8]
+
+for i in range(3, n + 1):
+    chu.append(chu[i - 1] + ka[i - 1] + pi[i - 1] - pi[i - 2])
+    ka.append(chu[i - 1] + ka[i - 1] + pi[i - 1] - chu[i - 3] - pi[i - 3])
+    pi.append(chu[i - 1] + ka[i - 1] + pi[i - 1] - chu[i - 3] - ka[i - 3])
+
+town_name = ''
+previous_syllable = ''
+current_syllable = ''
+
+for i in range(n - 1, -1, -1):
+    if k <= chu[i]:
+        current_syllable = 'chu'
+        town_name += current_syllable
+        if i >= 3:
+            ka[i - 1] -= pi[i - 2]
+    elif k <= chu[i] + ka[i]:
+        current_syllable = 'ka'
+        town_name += current_syllable
+        k -= chu[i]
+        if previous_syllable == 'ka':
+            ka[i - 1] = 0
+        elif i >= 3:
+            ka[i - 1] = ka[i - 1] - chu[i - 3] - pi[i - 3]
+    else:
+        current_syllable = 'pi'
+        town_name += current_syllable
+        k = k - chu[i] - ka[i]
+        if i >= 3:
+            pi[i - 1] = ka[i - 1] - chu[i - 3] - ka[i - 3]
+
+    previous_syllable = current_syllable
+
+print(town_name)
