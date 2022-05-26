52dbb6bb2eba0b72a87a03ed91ccf780dcd1dadf
Alexandr Solomevich <alexandr.solomevich@gmail.com>
2022-05-26 23:58:49 +0300

    solve for n^3

--- Algorithms and Data Structures/Park/main.cpp
+++ Algorithms and Data Structures/Park/main.cpp
@@ -1,156 +1,65 @@
-#include <fstream>
 #include <iostream>
-#include <algorithm>
+#include <fstream>
 #include <vector>
 #include <set>
-#include<ctime>
+#include <algorithm>
 
 using namespace std;
 
-static int area(int left, int top, int right, int bottom) {
-    cout << "Left: " << left << " Right: " << right << " Top: " << top << " Bottom: " << bottom << "\n\n\n";
+int CalculateArea(int right, int left, int top, int bottom) {
     return (right - left) * (top - bottom);
 }
 
+bool TreeComparator (vector <int> first, vector <int> second) {
+    return first[0] < second[0];
+}
+
 int main() {
-    long startTime = clock();
+    fstream input_file("input.txt");
 
-    FILE *pFile = fopen("input.txt", "r");
-    FILE *output = fopen("output.txt", "w");
+    int n_trees, width, height;
+    input_file >> n_trees >> width >> height;
 
-    int n = 0, c = 0, r = 0;
-    fscanf(pFile, "%d", &n);
-    fscanf(pFile, "%d", &c);
-    fscanf(pFile, "%d", &r);
-
-    int maxArea = 0;
-
-    vector<int> xPointCoordinate;
-    vector<int> yPointCoordinate;
-
-    set<int> yCoordinate;
+    vector<vector<int>> trees(n_trees, vector(2, 0));
+    set<int> unique_y;
 
     int x, y;
-    for (int i = 0; i < n; ++i)
-    {
-        fscanf(pFile, "%d", &x);
-        fscanf(pFile, "%d", &y);
-        xPointCoordinate.push_back(x);
-        yPointCoordinate.push_back(y);
+    for (int i = 0; i < n_trees; ++i) {
+        input_file >> x >> y;
+        trees[i][0] = x;
+        trees[i][1] = y;
+        unique_y.insert(y);
+    }
+    unique_y.insert(0);
+    unique_y.insert(height);
 
-        if (yCoordinate.find(y) == yCoordinate.end())
-        {
-            yCoordinate.insert(y);
-            xPointCoordinate.push_back(0);
-            yPointCoordinate.push_back(y);
-            xPointCoordinate.push_back(c);
-            yPointCoordinate.push_back(y);
+    vector<int> unique_y_vector(unique_y.begin(), unique_y.end());
+
+    sort(trees.begin(), trees.end(), TreeComparator);
+    sort(unique_y_vector.begin(), unique_y_vector.end());
+
+    int area = 0;
+
+    int left_border = 0;
+    int right_border = 0;
+    for (int i = unique_y_vector.size() - 1; i >= 0; --i) {
+        for (int j = unique_y_vector.size() - 1; j > i; --j) {
+            for (int k = 0; k < n_trees; ++k) {
+                if (trees[k][1] > unique_y_vector[i] && trees[k][1] < unique_y_vector[j] && right_border != trees[k][0]) {
+                    right_border = trees[k][0];
+                    area = max(area, CalculateArea(right_border, left_border, unique_y_vector[j], unique_y_vector[i]));
+                    left_border = right_border;
+
+                }
+            }
+            right_border = width;
+            area = max(area, CalculateArea(right_border, left_border, unique_y_vector[j], unique_y_vector[i]));
         }
     }
 
-    int size = xPointCoordinate.size();
-    int * topCoordinate = new int[size];
-    int * bottomCoordinate = new int[size];
-    bool * theSameYCoordinatePoint = new bool[size];
+    ofstream output_file("output.txt");
+    output_file << area;
 
-    for (int i = 0; i < size; ++i)
-    {
-        topCoordinate[i] = r;
-        bottomCoordinate[i] = 0;
-        theSameYCoordinatePoint[i] = false;
-    }
-
-    for (int i = 0; i < size; i++) {
-        cout << xPointCoordinate[i] << ' ';
-    }
-    cout << '\n';
-    for (int i = 0; i < size; i++) {
-        cout << yPointCoordinate[i] << ' ';
-    }
-    cout << "\n\n";
-
-
-    int temp;
-    //pointCoordinate.Sort((p1, p2) = > p1.X.CompareTo(p2.X));
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (xPointCoordinate[j] > xPointCoordinate[j + 1]) {
-                // меняем элементы местами
-                temp = xPointCoordinate[j];
-                xPointCoordinate[j] = xPointCoordinate[j + 1];
-                xPointCoordinate[j + 1] = temp;
-
-                temp = yPointCoordinate[j];
-                yPointCoordinate[j] = yPointCoordinate[j + 1];
-                yPointCoordinate[j + 1] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < size; i++) {
-        cout << xPointCoordinate[i] << ' ';
-    }
-    cout << '\n';
-    for (int i = 0; i < size; i++) {
-        cout << yPointCoordinate[i] << ' ';
-    }
-    cout << "\n\n";
-
-    if (n == 0) {
-        maxArea = r * c;
-    }
-
-
-    for (int i = (size - n) / 2; i < size; ++i)
-    {
-        maxArea = max(maxArea, area(xPointCoordinate[i - 1], topCoordinate[i], xPointCoordinate[i], bottomCoordinate[i]));
-        for (int j = 0; j < i; ++j)
-        {
-            if (yPointCoordinate[i] > yPointCoordinate[j] && yPointCoordinate[i] <= topCoordinate[j])
-            {
-                if (theSameYCoordinatePoint[j])
-                {
-                    maxArea = max(maxArea, area(xPointCoordinate[j], topCoordinate[j], xPointCoordinate[i], yPointCoordinate[j]));
-                }
-                else
-                {
-                    maxArea = max(maxArea, area(xPointCoordinate[j], topCoordinate[j], xPointCoordinate[i], bottomCoordinate[j]));
-                }
-                topCoordinate[j] = yPointCoordinate[i];
-            }
-            if (yPointCoordinate[i] < yPointCoordinate[j] && yPointCoordinate[i] >= bottomCoordinate[j])
-            {
-                if (theSameYCoordinatePoint[j])
-                {
-                    maxArea = max(maxArea, area(xPointCoordinate[j], yPointCoordinate[j], xPointCoordinate[i], bottomCoordinate[j]));
-                }
-                else
-                {
-                    ///
-                    maxArea = max(maxArea, area(xPointCoordinate[j], topCoordinate[j], xPointCoordinate[i], bottomCoordinate[j]));
-                }
-                bottomCoordinate[j] = yPointCoordinate[i];
-            }
-            else if (yPointCoordinate[i] == yPointCoordinate[j])
-            {
-                if (theSameYCoordinatePoint[j])
-                {
-                    maxArea = max(maxArea, area(xPointCoordinate[j], yPointCoordinate[i], xPointCoordinate[i], bottomCoordinate[j]));
-                    maxArea = max(maxArea, area(xPointCoordinate[j], topCoordinate[j], xPointCoordinate[i], yPointCoordinate[i]));
-
-                }
-                else
-                {
-                    maxArea = max(maxArea, area(xPointCoordinate[j], topCoordinate[j], xPointCoordinate[i], bottomCoordinate[j]));
-                }
-                theSameYCoordinatePoint[j] = true;
-            }
-        }
-    }
-
-    fprintf(output, "%d", maxArea);
-
-    long endTime = clock();
-    cout << endTime - startTime << endl;
-    return 0;
+    input_file.close();
+    output_file.close();
 }
